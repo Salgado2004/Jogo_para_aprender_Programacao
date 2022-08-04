965513a6d602089ad85fd2b272573f5a2fc14718
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-04 17:16:39 -0300

    Add files via upload

---
+++ resultado.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/resultado', methods=['POST'])
+def resultado() -> 'html':
+    ordem = [
+        {'index': int(request.form['condicionalIf']), 'valor': 'if('+request.form['condicaoIf']+'):\n'},
+        {'index': int(request.form['print1']), 'valor': '   print('+request.form['texto1']+')\n'},
+        {'index': int(request.form['print2']), 'valor': '   print('+request.form['texto2']+')\n'},
+        {'index': int(request.form['variavel']), 'valor': "qtdFrutas = 10\n"},
+        {'index': int(request.form['condicionalElse']), 'valor': "else:\n"}
+    ]
+    ordem.sort(key=ordena)
+    arquivoCodigo = open("codigoMissao1.py", "w")
+    for x in ordem:
+        arquivoCodigo.write(x['valor'])
+    return "o código foi baixado!"
+
+def ordena(e):
+  return e['index']
+
+@app.route('/teste2')
+def entry_page() -> 'html':
+    return render_template('teste2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
