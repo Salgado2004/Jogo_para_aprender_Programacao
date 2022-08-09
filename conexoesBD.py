2f92187617a2874e0c843f133dd5d523df749df4
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-09 18:53:33 -0300

    Add files via upload

---
+++ conexoesBD.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+dbconfig = {'host': '127.0.0.1', 'user': 'root', 'password': 'laura2312', 'database': 'mycodinggamedb'}
+
+def log(email, senha):
+    logmail = email
+    logsenha = senha
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    if logmail != "" and logsenha != "":
+        parametro = tuple(logmail)
+        _SQL = """select * from usuario where email = '"""+logmail+"""';"""
+        cursor.execute(_SQL)
+        res = cursor.fetchall()
+        if len(res) > 0:
+            for row in res:
+                id, nome, palavrapasse, nivel, emailLogin = row
+                print(row)
+                print(palavrapasse+" = "+logsenha)
+                if palavrapasse == logsenha:
+                    print(nome)
+                    return nome
+                else:
+                    print("Pois é "+nome+", a condição falhou")
+                    return nome
