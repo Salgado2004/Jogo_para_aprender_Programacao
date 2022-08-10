bec5bfbfc43d122d770e97ca6dedcb1d20222d32
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-08-10 15:21:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,17 +29,20 @@ def ordena(e):
 @app.errorhandler(404)
 def erro404(error):
     return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! Essa página não existe :/"), 404
+                            the_msg = "Opa! Essa página não existe :/",
+                            the_erro = "404"), 404
 
 @app.errorhandler(400)
 def erro404(error):
     return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! Parece que falta alguma coisa no seu código :/"), 400
+                            the_msg = "Opa! Parece que falta alguma coisa no seu código :/",
+                            the_erro = "400"), 400
 
 @app.errorhandler(405)
 def erro404(error):
     return render_template('GerenciadorErro.html',
-                            the_msg = "Opa! O método utilizado para acessar essa página não é válido :/"), 405
+                            the_msg = "Opa! O método utilizado para acessar essa página não é válido :/",
+                            the_erro = "405"), 405
 
 @app.route('/')
 def entry_page() -> 'html':
